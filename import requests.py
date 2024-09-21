db6909d85b345a49b9c9cca4ce59af18b5b20858
user3554 <sergeismolyak69@gmail.com>
2024-09-21 18:25:51 +0300

    third third

--- import requests.py
+++ import requests.py
@@ -9,6 +9,8 @@ from kivy.core.text import LabelBase
 from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
+import json
 """""
 localHost='http://172.31.1.203:3000'
 r = requests.get(localHost)
@@ -17,36 +19,37 @@ print(r.text)
 r=requests.post(localHost+"/login", data = {"username":"123", "password": "222"})
 print(r.text)
 """
+localHost='http://172.31.1.203:3000'
 path=r"C:\Users\user\Documents\clfr\clfr\1052.4.jpg"
 layout = FloatLayout()
 button = Button(text='Войти',font_size=40,background_color=(0,0,0, 1),size_hint =(.3, .10),pos =(672, 300))
 layout.add_widget(button)
-textinput = TextInput(text='login',font_size=40,size_hint =(.3, .10),pos =(672, 500), multiline=False)
-layout.add_widget(textinput)
-textinput1 = TextInput(text='password',font_size=40,size_hint =(.3, .10),pos =(672, 640), multiline=False)
-layout.add_widget(textinput1)
+logininput = TextInput(text='login',font_size=40,size_hint =(.3, .10),pos =(672, 640), multiline=False)
+layout.add_widget(logininput)
+passinput = TextInput(text='password',font_size=40,size_hint =(.3, .10),pos =(672, 500), multiline=False)
+layout.add_widget(passinput)
+l = Label(text='Вход',color=(0,0,0,1), font_size='50sp',size_hint =(.3, .10),pos =(672, 780))
+l2 = Label(text='',color=(0,0,0,1), font_size='30sp',size_hint =(.3, .10),pos =(672, 400))
+layout.add_widget(l)
+layout.add_widget(l2)
 
 
-    
-    
+
+def on_button_press(instance):
+        r=requests.post(localHost+"/login", data = {"login":logininput.text, "password": passinput.text})
+        l2.text=r.text
 """""
-class LoginPage(Screen):
-    def verify_credentials(self):
-        if self.ids["login"].text == "username" and self.ids["passw"].text == "password": #Сережа замени юзернейм и пассворд реквестами на бд это сравнение введённого и хранимого на бд
-            self.manager.current = "user"
-
-class UserPage(Screen):
-    pass
-
-class ScreenManagement(ScreenManager):
-    pass
-
-kv_file = Builder.load_file('login.kv')
-"""
+        button.bind(on_press=self.on_button_press)
+    
+    def on_button_press(self):
+        self.logininput.text = "Спасибо!"
+        """
 class MyApp(App):
+    button.bind(on_press=on_button_press)
     def build(self):
         Window.clearcolor = (255, 255, 255, 1)
-        return layout
+        
 
+        return layout
 if __name__ == '__main__':
     MyApp().run()
