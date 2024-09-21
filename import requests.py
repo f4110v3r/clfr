ebaf2d929720c7b47a875bf5d1c4d37ea161ab68
f4110v3r <111393058+f4110v3r@users.noreply.github.com>
2024-09-21 18:25:04 +0300

    п

--- import requests.py
+++ import requests.py
@@ -1,51 +1,54 @@
-import requests
 import kivy
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.lang.builder import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
-from kivy.core.text import LabelBase
-from kivy.lang import Builder
-from kivy.core.window import Window
 from kivy.uix.textinput import TextInput
-"""""
-localHost='http://172.31.1.203:3000'
-r = requests.get(localHost)
-print(r.text)
+from kivy.core.window import Window
+from kivy.graphics import Color, RoundedRectangle
 
-r=requests.post(localHost+"/login", data = {"username":"123", "password": "222"})
-print(r.text)
-"""
-path=r"C:\Users\user\Documents\clfr\clfr\1052.4.jpg"
-layout = FloatLayout()
-button = Button(text='Войти',font_size=40,background_color=(0,0,0, 1),size_hint =(.3, .10),pos =(672, 300))
-layout.add_widget(button)
-textinput = TextInput(text='login',font_size=40,size_hint =(.3, .10),pos =(672, 500), multiline=False)
-layout.add_widget(textinput)
-textinput1 = TextInput(text='password',font_size=40,size_hint =(.3, .10),pos =(672, 640), multiline=False)
-layout.add_widget(textinput1)
+class RoundedButton(Button):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas.before:
+            Color(0, 0, 0, 1)  # Цвет кнопки
+            self.rect = RoundedRectangle(size=self.size, pos=self.pos, radius=[20])
+        self.bind(size=self._update_rect, pos=self._update_rect)
 
+    def _update_rect(self, *args):
+        self.rect.pos = self.pos
+        self.rect.size = self.size
 
-    
-    
-"""""
-class LoginPage(Screen):
-    def verify_credentials(self):
-        if self.ids["login"].text == "username" and self.ids["passw"].text == "password": #Сережа замени юзернейм и пассворд реквестами на бд это сравнение введённого и хранимого на бд
-            self.manager.current = "user"
+class RoundedTextInput(TextInput):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas.before:
+            Color(1, 1, 1, 1)  # Цвет фона поля ввода
+            self.rect = RoundedRectangle(size=self.size, pos=self.pos, radius=[20])
+        self.bind(size=self._update_rect, pos=self._update_rect)
 
-class UserPage(Screen):
-    pass
+    def _update_rect(self, *args):
+        self.rect.pos = self.pos
+        self.rect.size = self.size
 
-class ScreenManagement(ScreenManager):
-    pass
+    def on_touch_down(self, touch):
+        if self.collide_point(touch.x, touch.y):
+            self.focus = True
+        return super().on_touch_down(touch)
 
-kv_file = Builder.load_file('login.kv')
-"""
 class MyApp(App):
     def build(self):
-        Window.clearcolor = (255, 255, 255, 1)
+        Window.clearcolor = (1, 1, 1, 1)  # Белый фон
+        layout = FloatLayout()
+
+        button = RoundedButton(text='Войти', font_size=40, size_hint=(.3, .1), pos=(672, 300))
+        layout.add_widget(button)
+
+        textinput = RoundedTextInput(hint_text='Логин', font_size=40, size_hint=(.3, .1), pos=(672, 500), multiline=False)
+        layout.add_widget(textinput)
+
+        textinput1 = RoundedTextInput(hint_text='Пароль', font_size=40, size_hint=(.3, .1), pos=(672, 640), multiline=False)
+        layout.add_widget(textinput1)
+
         return layout
 
 if __name__ == '__main__':
